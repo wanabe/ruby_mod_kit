eabbec5ae124b4d8760b2c60bc7b93d7f7fe149c
wanabe <s.wanabe@gmail.com>
2024-11-23 19:41:32 +0900

    feat: replace BaseNode#wrap with Node.wrap

--- spec/ruby_mod_kit/node_spec.rb
+++ spec/ruby_mod_kit/node_spec.rb
@@ -3,7 +3,7 @@
 require "ruby_mod_kit/cli"
 
 describe RubyModKit::Node do
-  let(:node) { RubyModKit::Node::ProgramNode.new(parse_result).wrap(prism_node) }
+  let(:node) { described_class.wrap(prism_node, parent: nil) }
   let(:parse_result) { Prism.parse(script).value }
 
   describe "#name" do
