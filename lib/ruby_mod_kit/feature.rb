b9b7cdd17547558f02c40ca56f0b755ce1be84e8
wanabe <s.wanabe@gmail.com>
2024-11-26 04:25:25 +0900

    feat: Remove Feature#<=>

--- lib/ruby_mod_kit/feature.rb
+++ lib/ruby_mod_kit/feature.rb
@@ -5,12 +5,6 @@
 module RubyModKit
   # the base class of feature
   class Feature
-    # @rbs other: Feature
-    # @rbs return: -1 | 0 | 1 | nil
-    def <=>(other)
-      self.class.name <=> other.class.name
-    end
-
     # @rbs return: Array[Corrector]
     def create_correctors
       []
