51bc62bb24bada0fd417863dec19db1075c5c3ff
wanabe <s.wanabe@gmail.com>
2024-11-28 17:31:50 +0900

    test: fix RubyModKit::Node spec to pass parent

--- spec/ruby_mod_kit/node_spec.rb
+++ spec/ruby_mod_kit/node_spec.rb
@@ -3,13 +3,14 @@
 require "ruby_mod_kit/cli"
 
 describe RubyModKit::Node do
-  let(:node) { described_class.wrap(prism_node, parent: nil) }
-  let(:parse_result) { Prism.parse(script).value }
+  let(:node) { described_class.wrap(prism_node, parent: parent) }
+  let(:parent) { RubyModKit::Node::ProgramNode.new(prism_root_node) }
+  let(:prism_root_node) { Prism.parse(script).value }
 
   describe "#name" do
     context "with Prism::DefNode" do
       let(:script) { "def foo; end" }
-      let(:prism_node) { parse_result.statements.body[0] }
+      let(:prism_node) { prism_root_node.statements.body[0] }
 
       it "returns method name" do
         expect(node.name).to eq(:foo)
@@ -18,7 +19,7 @@ describe RubyModKit::Node do
 
     context "with Prism::StatementsNode" do
       let(:script) { "def foo; end" }
-      let(:prism_node) { parse_result.statements }
+      let(:prism_node) { prism_root_node.statements }
 
       it "raises an exception" do
         expect { node.name }.to raise_error(RubyModKit::Error)
@@ -27,7 +28,7 @@ describe RubyModKit::Node do
 
     context "with Prism::RequiredParameterNode" do
       let(:script) { "def foo(bar); end" }
-      let(:prism_node) { parse_result.statements.body[0].parameters.requireds[0] }
+      let(:prism_node) { prism_root_node.statements.body[0].parameters.requireds[0] }
 
       it "returns parameter name" do
         expect(node.name).to eq(:bar)
@@ -37,7 +38,7 @@ describe RubyModKit::Node do
 
   describe "#inspect" do
     let(:script) { "def foo; end" }
-    let(:prism_node) { parse_result }
+    let(:prism_node) { prism_root_node }
 
     it "doesn't include redundant values @children, @parent nor @ancestors" do
       child = node.children[0]
@@ -50,7 +51,7 @@ describe RubyModKit::Node do
 
   describe "#parameter_node_at" do
     let(:script) { "def foo(bar); end" }
-    let(:prism_node) { parse_result }
+    let(:prism_node) { prism_root_node }
 
     it do
       expect(node.parameter_node_at(8)).to be_a(RubyModKit::Node::ParameterNode)
